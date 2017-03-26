c538d04470cb35b198b4e1a1ce6be16a691bf346
MNiestijl <milan_niestijl@hotmail.com>
2017-03-26 22:05:54 +0200

    Cant find the proper datasets :(

--- Exercise 3/SSLDA_Classifier.py
+++ Exercise 3/SSLDA_Classifier.py
@@ -7,8 +7,9 @@ from copy import copy
 
 class SSLDA_Classifier():
     def __init__(self, max_iter=10, n_components=2):
-        self.n_components = n_components
+        self.n_components, self.max_iter = n_components, max_iter
         self.covariance_, self.means_, self.classifier = None, None, None
+        self.propagated_labels = None
 
     def predict(self, X):
         return self.classifier.predict(X)
@@ -24,28 +25,35 @@ class SSLDA_Classifier():
     def fit(self, X, y, method='self-training', threshhold=0.7):
         getLabel = lambda p: np.where(p>threshhold)[0][0] if np.any(p>threshhold) else -1
         yp = copy(y) # copy of original labels
+        mask = np.ones(len(y), dtype=bool)
+        mask[np.where(yp==-1)[0]]=False
         lda = LinearDiscriminantAnalysis(solver='lsqr',store_covariance=True, n_components=2)
 
-        if method=='self-training':
+        if method=='none':
+            print('method is none')
+            lda.fit(X[mask,:], yp[mask])
+
+        elif method=='self-training':
+            print('method is self-training')
             counter = 0
             while True: # Temporary
-                mask = np.ones(len(y), dtype=bool)
-                mask[np.where(yp==-1)[0]]=False
-                if len(yp[~mask])==0 or counter==max_iter:
+                if len(yp[~mask])==0 or counter==self.max_iter:
                     break
                 lda.fit(X[mask,:],yp[mask])
                 probs = lda.predict_proba(X[~mask])
                 yp[~mask] = np.fromiter([getLabel(p) for p in probs], probs.dtype)
                 counter+=1
+                mask = np.ones(len(y), dtype=bool)
+                mask[np.where(yp==-1)[0]]=False
 
-        if method=='label-propagation':
+        elif method=='label-propagation':
+            print('method is label-propagation')
             label_prop_model = LabelPropagation(kernel='knn', n_neighbors=10, alpha=0.9)
             label_prop_model.fit(X,yp)
-            mask = np.ones(len(y), dtype=bool)
-            mask[np.where(yp==-1)[0]]=False
             probs = label_prop_model.predict_proba(X[~mask])
             yp[~mask] = np.fromiter([getLabel(p) for p in probs], probs.dtype)
-            print(len(np.where(yp==-1)[0]))
+            self.propagated_labels = yp
             lda.fit(X[mask,:],yp[mask])
-
+        else:
+            raise('No valid method was given!')
         self.classifier, self.means_, self.covariance_ = lda, lda.means_, lda.covariance_
