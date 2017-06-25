3d24635fd96d26500dec6dacbd08a166dedb3c0a
MNiestijl <milan_niestijl@hotmail.com>
2017-06-25 13:07:55 +0200

    ready to optimize hyperparameters

--- Final Assignment/CustomClassifiers.py
+++ Final Assignment/CustomClassifiers.py
@@ -16,7 +16,7 @@ class CC1(BaseEstimator, ClassifierMixin):
 	def __str__(self):
 		return "CC1"
 
-	def __init__(self,C1=LinearRegression(),C2=LinearRegression(),C3=LinearRegression(),labs=[1,2,3]):
+	def __init__(self,C1=SVC(),C2=SVC(),C3=SVC(),labs=[1,2,3]):
 		self.C1 = C1 	# binary classifier
 		self.C2 = C2	
 		self.C3 = C3
@@ -84,5 +84,29 @@ class CC2(BaseEstimator, ClassifierMixin):
 		return self.mainCL.predict(X2)
 
 	def score(self, X, y):
+		predicted = self.predict(X)
+		return np.mean([1 if predicted[i]==y[i] else 0 for i in range(0,len(y))])
+
+class SelfTrainer():
+
+	def __init__(self, BaseEstimator=SVC(), treshhold=None):
+		self.BaseEstimator = BaseEstimator
+
+	def fit(self,X,y):
+		Xlab = X[np.where(y!=-1),:]
+
+		return self
+
+	def predict(self,X):
+		pred1 = self.C1.predict(X)
+		ix1,ix2,X1,X2,_,_ = u.splitData(X,pred1,[1])
+		y1 = self.C2.predict(X1)
+		y2 = self.C3.predict(X2)
+		y = np.ones(X.shape[0])
+		y[ix1] = y1
+		y[ix2] = y2
+		return y
+
+	def score(self,X,y):
 		predicted = self.predict(X)
 		return np.mean([1 if predicted[i]==y[i] else 0 for i in range(0,len(y))])
--- Final Assignment/main.py
+++ Final Assignment/main.py
@@ -9,7 +9,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 import utils as u
-from CustomClassifier import CC1, CC2
+from CustomClassifiers import CC1, CC2
 
 # Settings
 #mpl.rcParams['text.usetex'] = True
@@ -27,22 +27,34 @@ TODO:
 	Repeat untill predicted labels on TEST converge or max_iter is reached.
 """
 
-def main():
-	Xtrn, Ytrn, Strn, Xtst, Xall, Yall,Xfull = u.getData()
+def OptimizeSVCParameters(X, Y, Cs, kernels, degrees=[3] cv=5):
+	combinations = [(C,kernel) for C in Cs for kernel in kernels]
+	allScores = {}
+	best = {}
+	bestScore = 0
+	for C, kernel in combinations:
+		if kernel=='poly':
+			for degree in degrees:
+			classifier = SVC(C=C, kernel=kernel, degree=degree)
+			scores = cross_val_score(classifier, X, Y, cv=cv)
+			allScores[(C,kernel, degree)] = { mean : scores.mean(), std : scores.std() }
+			if scores.mean()>bestScore:
+				best['C'] = C
+				best['kernel'] = kernel
+				best['degree'] = degree
+				bestScore = scores.mean()
+		else:
+			classifier = SVC(C=C, kernel=kernel)
+			scores = cross_val_score(classifier, X, Y, cv=cv)
+			allScores[(C,kernel, degree)] = { mean : scores.mean(), std : scores.std() }
+			if scores.mean()>bestScore:
+				best['C'] = C
+				best['kernel'] = kernel
+				bestScore = scores.mean()
+	return best, allScores
 
-	# Label active and unactive samples
-	labs = [1,2,3]
-	Yact = u.getActLabels(Ytrn,labs)
-	_,_, Xtrn1, Xtrn2, Ytrn1, Ytrn2 = u.splitData(Xtrn,Ytrn,labs)
 
-	# Make plots
-	#plotSingularValues(plt.figure(1),Xfull,N=10,normalize=True)
-	#plotPrincipalComponents(plt.figure(2),Xfull,Ytrn)
-	#plotPrincipalComponents(plt.figure(3),Xfull,Yact)
-	#plotPrincipalComponents(plt.figure(4),Xtrn1,Ytrn1)
-	#plotPrincipalComponents(plt.figure(5),Xtrn2,Ytrn2)
-	#plt.show()
-
+def test():
 	CL1 = lambda : SVC(C=1, kernel='rbf')
 	CL2 = lambda : SVC(C=10, kernel='poly', degree=2)
 	CL3 = lambda : SVC(C=10, kernel='poly', degree=3)
@@ -65,8 +77,37 @@ def main():
 		scores = cross_val_score(classifier,Xfull,Ytrn, cv=10)
 		print('\nScore = {} +/- {}'.format(scores.mean(), scores.std()))
 
+
+def main():
+	Xtrn, Ytrn, Strn, Xtst, Xall, Yall,Xfull = u.getData()
+
+	# Label active and unactive samples
+	labs = [1,2,3]
+	Yact = u.getActLabels(Ytrn,labs)
+	_,_, Xtrn1, Xtrn2, Ytrn1, Ytrn2 = u.splitData(Xtrn,Ytrn,labs)
+
+	# Make plots
+	#plotSingularValues(plt.figure(1),Xfull,N=10,normalize=True)
+	#plotPrincipalComponents(plt.figure(2),Xfull,Ytrn)
+	#plotPrincipalComponents(plt.figure(3),Xfull,Yact)
+	#plotPrincipalComponents(plt.figure(4),Xtrn1,Ytrn1)
+	#plotPrincipalComponents(plt.figure(5),Xtrn2,Ytrn2)
+	#plt.show()
+
+	# Find optimal parameters:
+	Cs = [1,3,5,7,10,13,15,17,20,24,27,30]
+	kernels = ['rbf', 'poly']
+	degrees = [1,2,3,4,5]
+
+	#best1, allScores1 = OptimizeSVCParameters(Xtrn1, Ytrn1, Cs, kernels, degrees, cv=10)
+	#best2, allScores2 = OptimizeSVCParameters(Xtrn2, Ytrn2, Cs, kernels, degrees, cv=10)
+
+
+
+	
+
 	# Make submission File
-	u.makeSubmissionFile(Xtrn, Ytrn, Xtst, CL3(), name="SVC_C_10_poly_3", override=False)
+	#u.makeSubmissionFile(Xtrn, Ytrn, Xtst, CL3(), name="SVC_C_10_poly_3", override=False)
 
 if __name__ == "__main__":
 	main()
